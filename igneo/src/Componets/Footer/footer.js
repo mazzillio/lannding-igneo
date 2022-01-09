408c9fcf7bbd2b131ec78bd04346050e352ca949
mazzillio <mateca500@gmail.com>
2022-01-09 19:50:36 -0300

    Término do footer

--- igneo/src/Componets/Footer/footer.js
+++ igneo/src/Componets/Footer/footer.js
@@ -1,6 +1,15 @@
+
 import styles from './footer.module.css'
 export function Footer(){
-    return <footer>
+    const redirect=(local)=>{
+        window.location.href=`#${local}`
+    }
+    return <footer className={styles.divFooter}>
         <p>Produzido por Estúdio Igneo</p>
+        <ul>
+            <li onClick={()=>redirect('sobre')}>Sobre nós</li>
+            <li onClick={()=>redirect('servicos')}>Serviços</li>
+            <li onClick={()=>redirect('portifolio')}>Portifólio</li>
+        </ul>
     </footer>
 }
